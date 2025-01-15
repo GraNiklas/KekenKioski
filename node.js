94b031e463b1cfd39dee7f6f599daa8e54816a07
Munghard <47939872+Munghard@users.noreply.github.com>
2025-01-15 16:09:09 +0200

    eka commit

---
+++ node.js
@@ -0,0 +1,44 @@
+// Express.js palvelin
+const path = require('path')
+const express = require('express')
+
+const PORT = process.env.PORT || 3000;
+const fs = require('fs').promises
+
+const app = express();
+
+const henkilokunta = require('./henkilokunta.json')
+const yritysEsittely = require('./yritysEsittely.json')
+const yhteystiedot = require('./yhteystiedot.json')
+
+
+app.get('/api/henkilo',(req, res) => {
+    res.json(henkilokunta);
+    })
+
+app.get('/api/yritys',(req, res) => {
+    res.json(yritysEsittely);
+    })
+
+app.get('/api/yhteystiedot',(req, res) => {
+    res.json(yhteystiedot);
+    })
+
+app.get('/api/getpin',async (req, res) => {
+    try {
+        const savedPin = await fs.readFile('./pin.txt','utf-8')    
+        res.send(savedPin)
+    } 
+    catch (error) {
+        console.error('Error reading file: ', error)
+        res.status(500).send('internal server error')
+        }
+    })
+
+const polku = path.join(__dirname, './public')
+
+app.use(express.static(polku))
+
+app.listen(PORT,()=>{
+    console.log('Server is up on port: ' + PORT);
+})
