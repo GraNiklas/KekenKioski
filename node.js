bf8c9ccd01420eef611b4ba3990659019e4c1564
Munghard <47939872+Munghard@users.noreply.github.com>
2025-01-15 18:01:53 +0200

    added some options

--- node.js
+++ node.js
@@ -1,51 +1,59 @@
-// Express.js palvelin
-const path = require('path')
-const express = require('express')
+// Express.js server
+const path = require('path');
+const express = require('express');
 const cors = require('cors');
+const fs = require('fs').promises;
 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Load JSON files
+const henkilokunta = require('./henkilokunta.json');
+const yritysEsittely = require('./yritysEsittely.json');
+const yhteystiedot = require('./yhteystiedot.json');
+
+// CORS Options
 const corsOptions = {
-    origin: 'https://keken-kioski.onrender.com', // Allow requests from this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-  };
-  const PORT = process.env.PORT || 3000;
-  const fs = require('fs').promises
-  
-  const app = express();
-  
-  const henkilokunta = require('./henkilokunta.json')
-  const yritysEsittely = require('./yritysEsittely.json')
-  const yhteystiedot = require('./yhteystiedot.json')
-  
+    origin: 'https://keken-kioski.onrender.com', // Allow this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+};
+
+// Use CORS Middleware
 app.use(cors(corsOptions));
 
-app.get('/api/henkilo',(req, res) => {
+// Handle JSON responses
+app.get('/api/henkilo', (req, res) => {
     res.json(henkilokunta);
-    })
+});
 
-app.get('/api/yritys',(req, res) => {
+app.get('/api/yritys', (req, res) => {
     res.json(yritysEsittely);
-    })
+});
 
-app.get('/api/yhteystiedot',(req, res) => {
+app.get('/api/yhteystiedot', (req, res) => {
     res.json(yhteystiedot);
-    })
+});
 
-app.get('/api/getpin',async (req, res) => {
+app.get('/api/getpin', async (req, res) => {
     try {
-        const savedPin = await fs.readFile('./pin.txt','utf-8')    
-        res.send(savedPin)
-    } 
-    catch (error) {
-        console.error('Error reading file: ', error)
-        res.status(500).send('internal server error')
-        }
-    })
+        const savedPin = await fs.readFile('./pin.txt', 'utf-8');
+        res.send(savedPin);
+    } catch (error) {
+        console.error('Error reading file: ', error);
+        res.status(500).send('Internal server error');
+    }
+});
 
-const polku = path.join(__dirname, './public')
+// Serve static files
+const polku = path.join(__dirname, './public');
+app.use(express.static(polku));
 
-app.use(express.static(polku))
+// Handle Pre-flight Requests (OPTIONS)
+app.options('*', cors(corsOptions));
 
-app.listen(PORT,()=>{
+// Start Server
+app.listen(PORT, () => {
     console.log('Server is up on port: ' + PORT);
-})
+});
